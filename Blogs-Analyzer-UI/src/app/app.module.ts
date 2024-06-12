8df326e67e97d1cb1389e7f86fbe16496ccd2ab1
DeepaMittal <deepamittalmca@gmail.com>
2024-06-13 03:32:19 +0530

    added dashboard changes

--- Blogs-Analyzer-UI/src/app/app.module.ts
+++ Blogs-Analyzer-UI/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './dashboard/components/header/header.component';
+import {MaterialModule} from "./shared/module/material.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
