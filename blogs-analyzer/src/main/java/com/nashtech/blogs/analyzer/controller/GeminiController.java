7810519113a08aa8876d13df62510d283d30ddd2
Saurabh Dhotre <91465827+SaurabhDhotreknoldus@users.noreply.github.com>
2024-06-24 13:07:28 +0530

    Feature/read me update (#9)
    * Documentation Edit
    
    * Documentation Edit
    
    * Chat session History
    
    ---------
    
    Co-authored-by: aiupbw3_amway <Saurabh.Dhotre@Amway.com>

--- blogs-analyzer/src/main/java/com/nashtech/blogs/analyzer/controller/GeminiController.java
+++ blogs-analyzer/src/main/java/com/nashtech/blogs/analyzer/controller/GeminiController.java
@@ -2,15 +2,19 @@ package com.nashtech.blogs.analyzer.controller;
 
 
 import com.google.cloud.vertexai.api.GenerateContentResponse;
+import com.google.cloud.vertexai.api.Content;
 import com.google.cloud.vertexai.generativeai.ChatSession;
 import com.google.cloud.vertexai.generativeai.ResponseHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/gemini")
@@ -23,4 +27,12 @@ public class GeminiController {
         GenerateContentResponse generateContentResponse = this.chatSession.sendMessage(prompt);
         return ResponseHandler.getText(generateContentResponse);
     }
+
+    @GetMapping("/v1/history/{text}")
+    public List<String> getChatSessionHistory(@PathVariable String text) throws IOException {
+        GenerateContentResponse generateContentResponse = this.chatSession.sendMessage(text);
+        List<Content> history = this.chatSession.getHistory();
+        return history.stream().flatMap(h -> h.getPartsList().stream()).map(part -> part.getText()).toList();
+    }
+    
 }
