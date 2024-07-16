28dc331abd595630d1c3a0298dadf4f901d96154
kuldeep <kuldeep.swaroop@nashtechglobal.com>
2024-07-16 13:51:26 +0530

    updated backend endpoint

--- blogs-analyzer-ui/src/environments/environment.prod.ts
+++ blogs-analyzer-ui/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { NgxLoggerLevel } from "ngx-logger";
 
 export const environment = {
   production: true,
-  apiUrl: 'http://34.100.166.202/api',
+  apiUrl: 'http://35.244.36.216/api',
   logLevel: NgxLoggerLevel.DEBUG,
   serverLogLevel: NgxLoggerLevel.ERROR
 };
